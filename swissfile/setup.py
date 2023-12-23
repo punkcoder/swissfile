e891b1b283c92014b848dd42625678624737b1d6
James McKee <james.mckee.jr@gmail.com>
2023-12-23 16:55:52 -0600

    Moving over to click to make things faster and easier.

---
+++ swissfile/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='swissfile',
+    version='0.1',
+    py_modules=['swissfile'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        swissfile=swissfile:cli
+    ''',
+)
