d3c1f0746e5bfb1593a81bb2ddef3498482f7da7
James McKee <james.mckee.jr@gmail.com>
2023-12-24 11:50:41 -0600

    Add tag functionality and tests

--- swissfile/swissfile.py
+++ swissfile/swissfile.py
@@ -7,11 +7,42 @@ and dealing with large numbers of files.
 import click
 import logging
 import os
+import re
+
+tag_regex = re.compile(r"^[a-zA-Z0-9_]+\=[a-zA-Z0-9_]+$")
 
 
 @click.group()
 def cli():
     pass
+
+
+def verify_tag(tag):
+    """
+    Verify that the tag is in the correct format.
+    """
+    if not tag:
+        return {"error": "Tag cannot be empty."}
+    if len(tag) > 255:
+        return {"error": "Tag cannot be longer than 255 characters."}
+    if not tag_regex.match(tag):
+        return {"error": "Tag must be in the format key=value."}
+
+    return True
+
+
+def add_tag_to_file(file, tag):
+    """
+    Add a tag to a file.
+    """
+    if os.path.exists(file):
+        with open(f"{file}.tagdata", "r") as f:
+            if tag in f.read():
+                return {}
+
+        with open(f"{file}.tagdata", "a") as f:
+            # check to see if the tag is already in the file
+            f.write(f"\n{tag}")
 
 
 @cli.command()
@@ -30,22 +61,31 @@ def tag(path: str, tag: str):
     if not os.path.exists(path):
         return {"error": f"File {path} does not exist."}
 
+    if not verify_tag(tag):
+        return {"error": verify_tag(tag)}
+
+    # check to see if the supplied path value is a file or a directory
+    if os.path.isfile(path):
+        # if it is a file then add the tag to the file
+        add_tag_to_file(path, tag)
+
 
 @cli.command()
 @click.option("--path", default=".", help="Path to file.")
 def untag(path: str, tag: str):
-	"""
-	Remove a tag from a file.
+    """
+    Remove a tag from a file.
 
-	path of the file to tag
-	tag to add to the file
+    path of the file to tag
+    tag to add to the file
 
-	"""
-	print("Removing a tag from a file...")
+    """
+    print("Removing a tag from a file...")
 
-	# check to see if the file exists is it doesnt then return an error
-	if not os.path.exists(path):
-		return {"error": f"File {path} does not exist."}
+    # check to see if the file exists is it doesnt then return an error
+    if not os.path.exists(path):
+        return {"error": f"File {path} does not exist."}
+
 
 if __name__ == "__main__":
     cli()
--- tests/test_swissfile.py
+++ tests/test_swissfile.py
@@ -21,19 +21,71 @@ def execute_before_any_test():
 def test_tag_import():
     from swissfile.swissfile import tag
 
+
 def test_untag_import():
     from swissfile.swissfile import untag
 
 
 def test_untag_missing_file(cli_runner):
     from swissfile.swissfile import untag
+
     result = cli_runner.invoke(untag, ["--path", "tests/missing.txt", "--tag", "test"])
-    assert result.exit_code != 0 
-    
-    
+    assert result.exit_code != 0
+
+
 def test_tag_missing_file(cli_runner):
     from swissfile.swissfile import tag
+
     result = cli_runner.invoke(tag, ["--path", "tests/missing.txt", "--tag", "test"])
     assert result.exit_code != 0
-    
-    
+
+
+def test_verify_tag_empty():
+    from swissfile.swissfile import verify_tag
+
+    result = verify_tag("")
+    assert result["error"] == "Tag cannot be empty."
+
+
+def test_verify_tag_too_long():
+    from swissfile.swissfile import verify_tag
+
+    result = verify_tag("a" * 256)
+    assert result["error"] == "Tag cannot be longer than 255 characters."
+
+
+def test_verify_tag_invalid_format():
+    from swissfile.swissfile import verify_tag
+
+    result = verify_tag("test")
+    assert result["error"] == "Tag must be in the format key=value."
+
+
+def test_verify_tag_valid():
+    from swissfile.swissfile import verify_tag
+
+    result = verify_tag("test=test")
+    assert result == True
+
+
+def test_add_tag_to_file(cli_runner):
+    from swissfile.swissfile import add_tag_to_file
+
+    add_tag_to_file("tests/testfile.txt", "test=test")
+    with open("tests/testfile.txt.tagdata", "r") as f:
+        assert "test=test" in f.read()
+
+
+def test_tag_add_repeat_tag(cli_runner):
+    from swissfile.swissfile import tag
+
+    result = cli_runner.invoke(
+        tag, ["--path", "tests/testfile.txt", "--tag", "test=test"]
+    )
+
+    result = cli_runner.invoke(
+        tag, ["--path", "tests/testfile.txt", "--tag", "test=test"]
+    )
+    # check that the test=test tag is not added twice
+    with open("tests/testfile.txt.tagdata", "r") as f:
+        assert f.read().count("test=test") == 1
